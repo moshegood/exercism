9ab47bdae18b99927ea5d06327772e3871948836
Moshe Good <moshe@squareup.com>
2021-05-09 11:35:03 -0400

    Added solution to the go/two-fer problem

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,10 @@
+// Package twofer package comment here
+package twofer
+
+// ShareWith returns stuff.
+func ShareWith(name string) string {
+	if len(name) == 0 {
+		name = "you"
+	}
+	return "One for " + name + ", one for me."
+}
